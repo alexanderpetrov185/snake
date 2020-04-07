2501ef019f4572e925118792f72a9ec33dc1d111
Александр <banan7280@gmail.com>
2020-04-07 19:54:10 +0300

    6

--- snake/Program.cs
+++ snake/Program.cs
@@ -17,25 +17,32 @@ namespace snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Move(p1, 10, 10);
-            Console.WriteLine($"Move(p1, 10, 10) p1.x = {p1.x}, p1.y = {p1.y}");
+            List<int> numlist = new List<int>
+            {
+                3,
+                4,
+                5
+            };
 
-            Reset(p2);
-            Console.WriteLine($"Reset(p2) p2.x = {p2.x}, p2.y = {p2.y}");
+            int x = numlist[0];
+            int y = numlist[1];
+            int z = numlist[2];
 
-            Console.ReadKey();
-        }
+            foreach (int i in numlist)
+            {
+                Console.WriteLine(i);
+            }
 
-        private static void Reset(Point p)
-        {
-            p = new Point(6, 7, '@');
-            //  Здесь переменная p2 останется без изменений.
-        }
+            numlist.RemoveAt(0);
 
-        private static void Move(Point p, int dx, int dy)
-        {
-            p.x = p.x + dx;
-            p.y = p.y + dy;
+            List<Point> plist = new List<Point>
+            {
+                p1,
+                p2
+            };
+            
+
+            Console.ReadLine();
         }
     }
 }
