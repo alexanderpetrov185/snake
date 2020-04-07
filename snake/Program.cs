6ff089ae7bb5dc8e03cf182b1a6965c8372d4e44
Александр <banan7280@gmail.com>
2020-04-07 19:37:57 +0300

    5

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,14 +10,32 @@ namespace snake
     {
         static void Main(string[] args)
         {
+
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Console.ReadLine();
+            Move(p1, 10, 10);
+            Console.WriteLine($"Move(p1, 10, 10) p1.x = {p1.x}, p1.y = {p1.y}");
+
+            Reset(p2);
+            Console.WriteLine($"Reset(p2) p2.x = {p2.x}, p2.y = {p2.y}");
+
+            Console.ReadKey();
         }
 
+        private static void Reset(Point p)
+        {
+            p = new Point(6, 7, '@');
+            //  Здесь переменная p2 останется без изменений.
+        }
+
+        private static void Move(Point p, int dx, int dy)
+        {
+            p.x = p.x + dx;
+            p.y = p.y + dy;
+        }
     }
 }
